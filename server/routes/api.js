5cd558ece524067ebf7c1258e4ca87eff86238f0
Ashish Shah <ashish@wemodifi.com>
2017-03-04 22:15:42 -0800

    api file

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,6 +5,10 @@ router.get('/test', (req, res) => {
   res.send(200, 'test api called');
 });
 
+router.get('/orgs', (req, res) => {
+  // get a list of organizations
+});
+
 router.get('*', (req, res) => {
   res.send(404, 'file not found..!');
 })
